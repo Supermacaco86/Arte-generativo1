5b6ec42790e1d4c73ef8953a62a92fdbafd0dc4e
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2023-02-20 16:37:31 -0300

    Second commit

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+   background(255, 0, 0, 15);
+   stroke( 0, 0, 200);// Esto le da color a las lineas
+  line(0, 0, mouseX, mouseY);
+  line(width, 0, mouseX, mouseY);
+  line(0, height, mouseX, mouseY);
+  line(width, height, mouseX, mouseY);
+
+  fill(0, 200, 0);// Esto le da color a la elipse
+  ellipse(mouseX, mouseY, 20); 
+}
